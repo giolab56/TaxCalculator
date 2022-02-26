91e140acdaba395cab4caa5652c45d8dfbafe379
giolab56 <100443438+giolab56@users.noreply.github.com>
2022-02-25 20:11:34 -0500

    Add files via upload

---
+++ tax/main.py
@@ -0,0 +1,11 @@
+#This is a Tax Calculator
+
+gross_income = float(input("Enter the gross income: "))
+tax = 0.20
+tax_deduction = 10000
+dependents = int(input("Enter the number of dependents: "))
+dep_deduction = dependents * 3000
+
+income_tax = (gross_income - (dep_deduction + tax_deduction)) * tax
+
+print("The income tax is $" , income_tax)
